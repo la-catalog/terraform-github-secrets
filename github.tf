0213abdd835c068105f1b42618e1543bd39e82d1
thiagola92 <thiagola92@gmail.com>
2022-09-17 14:36:28 -0300

    Stop adding github_token to secrets from repositories

--- github.tf
+++
@@ -1,6 +0,0 @@
-resource "github_actions_secret" "gh_token" {
-  for_each        = toset(data.github_repositories.terraform.names)
-  repository      = each.key
-  secret_name     = "gh_token"
-  plaintext_value = var.gh_token
-}
