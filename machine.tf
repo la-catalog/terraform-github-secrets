049fc65b8738995d9b0a054c6099823506f75083
thiagola92 <thiagola92@gmail.com>
2022-10-13 07:32:08 -0300

    Remove machine_user, it's not necessary after copying your key to machine

--- machine.tf
+++ machine.tf
@@ -3,21 +3,9 @@ variable "machine_ip" {
   sensitive = true
 }
 
-variable "machine_user" {
-  type      = string
-  sensitive = true
-}
-
 resource "github_actions_secret" "machine_ip" {
   for_each        = toset(["terraform-services", "terraform-github-secrets"])
   repository      = each.key
   secret_name     = "machine_ip"
   plaintext_value = var.machine_ip
-}
-
-resource "github_actions_secret" "machine_user" {
-  for_each        = toset(["terraform-services", "terraform-github-secrets"])
-  repository      = each.key
-  secret_name     = "machine_user"
-  plaintext_value = var.machine_user
 }
